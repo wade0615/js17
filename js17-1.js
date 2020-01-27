a35cafb811da9fa920678d29d2eaf1c1e92e2bbf
Wade <wsw0615@gmail.com>
2020-01-27 21:39:06 +0800

    feat: js17-1 first try

--- js17-1.js
+++ js17-1.js
@@ -1,5 +1,15 @@
+// 好想電影院規定除非完全沒有客人買票，否則就算只有一位顧客也照常播放電影。
+// 讓使用者輸入客人的人數，並用「!」判斷人數，不為零顯示「照常播放電影」。
+
 
 function a (){
-    console.log("哈囉你成功了！")
+    console.log("歡迎光臨好想電影院")
+    console.log("請問你想買幾張電影票？")
+    process.stdin.once('data',function(data){
+        if(data == 2){
+            console.log("我進來了")
+        }
+    }
+    )
 }
 a()
