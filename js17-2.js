5f6a641996ffe4b6a63236902229600ca6f2ba67
Wade <wsw0615@gmail.com>
2020-01-31 11:08:12 +0800

    2-feat: not correct but success get two number

--- js17-2.js
+++ js17-2.js
@@ -2,6 +2,7 @@
 // 判斷餘數是否相同，若相同，則於螢幕上顯示「餘數相同」。
 
 remainder()
+// b()
 
 function remainder(){
     console.log("我會幫你判斷兩個數字的餘數是不是一樣喔");
@@ -14,13 +15,30 @@ function remainder(){
         var PositiveInteger = /^([1-9][0-9]*)$/; // var 一個正則表達式為判別是否為 正整數
         var StrToNun = parseInt(data, 10); // 將輸入的值從 字串string 轉乘 數字number,並指定為10進位
 
-        if(PositiveInteger.test(StrToNun)){
-            console.log("好的沒問題～接下來請輸入第二個數字：");
-            process.exit();
-        }else{
+        console.log("???");
+        if(!PositiveInteger.test(StrToNun)){
             console.log("請勿輸入0,負數或是其他字母！");
+        }else{
+            console.log("輸入正確");
+            // process.exit();
+            b();
         }
     }
     )
+}
+
+function b(){
+    console.log("接下來請輸入第二個數字：");
+    process.stdin.on('data',function(data2){
+        var PositiveInteger = /^([1-9][0-9]*)$/;
+        var StrToNun = parseInt(data2, 10);
+        if(!PositiveInteger.test(StrToNun)){
+            console.log("喔喔！再試一次囉！");
+        }else{
+            console.log("我收到兩個正確的數啦～掰囉～");
+            console.log(data2);
+            process.exit();
+        }
+    })
 }
 
