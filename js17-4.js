b832a2a36e3e54601403304de4ae7fdac5d882b0
Wade <wsw0615@gmail.com>
2020-02-03 21:40:03 +0800

    4-feat: what 02/03 haved done

--- js17-4.js
+++ js17-4.js
@@ -2,4 +2,51 @@
 
 console.log("我會幫你計算 1+2-3+4-5+6…n 的總和");
 console.log("這邊請你輸入 n 的項次:");
-calculate(0);
+// ReadInput();
+main();
+
+// function main() {
+//     var b = ReadInput();
+//     console.log("jj");
+//     console.log(b);
+// }
+
+function main() {
+    var b = ReadInput();
+    console.log("jj");
+    console.log(b);
+}
+
+async function ReadInput() {
+    process.stdin.setEncoding('utf8'); // 設定從可讀流讀取的字符編碼為utf8
+    var f = await process.stdin.once('data', function (inputnum) {
+        let StrToNun = parseInt(inputnum, 10); // 將輸入的值從 字串string 轉乘 數字number,並指定為10進位
+
+        var n = judgment(StrToNun);
+        console.log('hh');
+        console.log(n);
+        return n;
+    });
+    console.log("ff");
+    console.log(f);
+    return f;
+}
+
+function judgment(data) {
+    var PositiveInteger = /^([1-9][0-9]*)$/; // var 一個正則表達式為判別是否為 正整數
+
+    if (!PositiveInteger.test(data)) {
+        console.log("請勿輸入0,負數或是其他字母！");
+        ReadInput();
+    } else {
+        console.log("輸入正確");
+        // console.log(data);
+        return data;
+    }
+}
+
+function calculate() {
+    var b = ReadInput();
+    console.log("jj");
+    console.log(b);
+}
