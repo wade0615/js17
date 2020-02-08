552e034d12dfe519fcb46f357c3da87a4dce60f9
Wade <wsw0615@gmail.com>
2020-02-08 16:17:19 +0800

    feat: nothing

--- js17-4.js
+++ js17-4.js
@@ -3,13 +3,13 @@
 // console.log("我會幫你計算 1+2-3+4-5+6…n 的總和");
 console.log("我會幫你計算 1+2+3+4+5+6…n 的總和");
 console.log("這邊請你輸入 n 的項次:");
-main();
+a();
 
-function main() {
-    ReadInput();
+function a() {
+    main();
 }
 
-async function ReadInput() {
+async function main() {
     process.stdin.setEncoding('utf8'); // 設定從可讀流讀取的字符編碼為utf8
     var f = await process.stdin.once('data', function (inputnum) {
         let StrToNun = parseInt(inputnum, 10); // 將輸入的值從 字串string 轉乘 數字number,並指定為10進位
@@ -28,7 +28,7 @@ function judgment(data) {
 
     if (!PositiveInteger.test(data)) {
         console.log("請勿輸入0,負數或是其他字母！");
-        ReadInput();
+        main();
     } else {
         console.log("輸入正確");
         // console.log(data);
