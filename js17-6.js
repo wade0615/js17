47a389c582e6bdfdb8242c63b24bfcb58ba0fb1c
Wade <wsw0615@gmail.com>
2020-02-10 11:12:49 +0800

    6-feat: js17-6

---
+++ js17-6.js
@@ -0,0 +1,24 @@
+// 宣告陣列 name，讓使用者自行輸入四個英文名字於陣列中
+// 輸出第一個和第三個名字的單數個字母。
+
+console.log("請依序輸入四個英文名字")
+console.log("第一跟英文名字是：")
+// main();
+var name = ['bread', 'milk', 'cheese'];
+
+function a() {
+    // ReadInput();
+    dd = array(name);
+    console.log(dd)
+}
+
+function main() {
+    process.stdin.setEncoding('utf8'); // 設定從可讀流讀取的字符編碼為utf8
+    process.stdin.once('data',function () {
+        
+    })
+}
+
+function array(object) {
+    // object[0] = 'msx125';
+}
